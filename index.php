1f4c460ada3dade223c0098d8a0e46723ec0fe24
Javi <101107711+Javviviii2@users.noreply.github.com>
2022-11-06 22:37:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Karen Spärck Jones</title>
+    <link rel="stylesheet" href="./css/index.css" />
+    <link rel="stylesheet" href="./css/navbar.css" />
+  </head>
+  <body>
+    <header>
+      This is a header
+    </header>
+    <nav>
+      <div class="navbar">
+        <a href="./bibliografia.html">Home</a>
+      </div>
+    </nav>
+    <div class="grid">
+      <div class="hero">
+        <div class="hero_img"></div>
+        <div class="hero_text">
+          <h1>Karen Spärck Jones</h1>
+          <q>Words stand only for themselves</q>
+        </div>
+      </div>
+    </div>
+    <footer>
+      This is a footer
+      <? php ?>
+    </footer>
+  </body>
+</html>
